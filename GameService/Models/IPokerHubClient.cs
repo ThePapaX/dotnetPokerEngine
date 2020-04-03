30c6ca21f21a22add8f00477de3f3530621cbfe6
Armando Lozada <arloznav.sis@gmail.com>
2020-04-03 19:39:46 +1100

    Defined SignalR clientHub and serverHub interfaces

---
+++ GameService/Models/IPokerHubClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace GameService.Models
+{
+    public interface IPokerHubClient
+    {
+        Task ExecutePlayerAction(PlayerEvent action);
+        Task SendMessage(string senderId, string message);
+    }
+}
---
+++ GameService/Models/IPokerHubServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameService.Models
+{
+    public interface IPokerHubServer
+    {
+        Task ProcessClientAction(PlayerEvent playerAction);
+        Task DispatchGameEvent(GameEvent gameEvent);
+    }
+}
