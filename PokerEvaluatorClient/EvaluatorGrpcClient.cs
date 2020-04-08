793567cdfa0bd19ca39a56bee46f4e099c417f7b
Armando Lozada <arloznav.sis@gmail.com>
2020-04-08 22:05:58 +1000

    Added C# gRpc PokerEvaluatorClient

---
+++ PokerEvaluatorClient/EvaluatorGrpcClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Grpc.Core;
+
+namespace PokerEvaluatorClient
+{
+    public class EvaluatorGrpcClient
+    {
+
+        public EvaluatorGrpcClient()
+        {
+
+        }
+    }
+}
---
+++ PokerEvaluatorClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Core;
+using PokerEvaluator;
+using System;
+
+namespace PokerEvaluatorClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+
+            var client = new Evaluator.EvaluatorClient(channel);
+            String command = "pokenum -h As Ac - 2h 9d - Ks Kd -- 5s 4h 3h 7c 4c";
+
+            EvaluationResult result = client.Evaluate(new EvaluationRequest { Command = command });
+
+            Console.WriteLine("Result: " + result.Result);
+
+            channel.ShutdownAsync().Wait();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
