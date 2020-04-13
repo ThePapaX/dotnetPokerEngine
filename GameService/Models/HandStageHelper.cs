7c25b5eb330c2bc68a91473da3702f8c05fc4bdb
Armando Lozada <arloznav.sis@gmail.com>
2020-04-13 21:57:34 +1000

    Updated HandStateMachine class and test cases.

---
+++ GameService/Models/HandStageHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameService.Models
+{
+    public enum HandStage
+    {
+        Preflop,
+        Flop,
+        Turn,
+        River,
+        Finished
+    }
+    public class HandStageHelper
+    {
+        private readonly LinkedList<HandStage> Stage;
+        private LinkedListNode<HandStage> CurrentStage;
+        public HandStage GetCurrentStage() => CurrentStage.Value;
+
+        public void Reset() => CurrentStage = Stage.First;
+        public HandStage MoveNext()
+        {
+            if(CurrentStage.Value == HandStage.Finished)
+            {
+                throw new InvalidOperationException("CANNOT_MOVE_NEXT_BECAUSE_HAND_STATE_IS_FINISHED");
+            }
+
+            CurrentStage = CurrentStage.Next;
+            return CurrentStage.Value;
+        }
+    
+        public HandStageHelper()
+        {
+            Stage = new LinkedList<HandStage>();
+
+            Stage.AddFirst(HandStage.Preflop);
+            Stage.AddLast(HandStage.Flop);
+            Stage.AddLast(HandStage.Turn);
+            Stage.AddLast(HandStage.River);
+            Stage.AddLast(HandStage.Finished);
+
+            CurrentStage = Stage.First;
+        }
+        
+    }
+}
--- GameServiceTests/HandStateMachineTests.cs
+++ GameServiceTests/HandStateMachineTests.cs
@@ -11,40 +11,45 @@ namespace GameServiceTests
         [Test]
         public void CanBeInstantiated()
         {
-            var stateMachine = new HandStateMachine();
+            var stateMachine = new HandStageHelper();
             Assert.Pass();
         }
 
         [Test]
         public void TheInitialStateIsPreFlop()
         {
-            var stateMachine = new HandStateMachine();
-            Assert.AreEqual(HandState.Preflop, stateMachine.GetCurrentStateValue());
+            var stateMachine = new HandStageHelper();
+            Assert.AreEqual(HandStage.Preflop, stateMachine.GetCurrentStage());
         }
 
         [Test]
         public void MoveNextReturnTheNextStateInOrder()
         {
-            var stateMachine = new HandStateMachine();
+            var stateMachine = new HandStageHelper();
             var nextState = stateMachine.MoveNext();
 
-            Assert.AreEqual(HandState.Flop, stateMachine.GetCurrentStateValue());
-            Assert.AreEqual(HandState.Flop, nextState);
+            Assert.AreEqual(HandStage.Flop, stateMachine.GetCurrentStage());
+            Assert.AreEqual(HandStage.Flop, nextState);
 
             nextState = stateMachine.MoveNext();
-            Assert.AreEqual(HandState.Turn, stateMachine.GetCurrentStateValue());
-            Assert.AreEqual(HandState.Turn, nextState);
+            Assert.AreEqual(HandStage.Turn, stateMachine.GetCurrentStage());
+            Assert.AreEqual(HandStage.Turn, nextState);
 
             nextState = stateMachine.MoveNext();
-            Assert.AreEqual(HandState.River, stateMachine.GetCurrentStateValue());
-            Assert.AreEqual(HandState.River, nextState);
+            Assert.AreEqual(HandStage.River, stateMachine.GetCurrentStage());
+            Assert.AreEqual(HandStage.River, nextState);
+
+            nextState = stateMachine.MoveNext();
+            Assert.AreEqual(HandStage.Finished, stateMachine.GetCurrentStage());
+            Assert.AreEqual(HandStage.Finished, nextState);
         }
 
         [Test]
         public void MoveNextAfterRiverReturnsInvalidOperationExpection()
         {
-            var stateMachine = new HandStateMachine();
+            var stateMachine = new HandStageHelper();
             var nextState = stateMachine.MoveNext();
+            nextState = stateMachine.MoveNext();
             nextState = stateMachine.MoveNext();
             nextState = stateMachine.MoveNext();
             Assert.Throws(Is.TypeOf<InvalidOperationException>(), delegate { stateMachine.MoveNext(); });
@@ -53,12 +58,12 @@ namespace GameServiceTests
         [Test]
         public void HandCanBeReset()
         {
-            var stateMachine = new HandStateMachine();
+            var stateMachine = new HandStageHelper();
             stateMachine.MoveNext();
             stateMachine.MoveNext();
             stateMachine.Reset();
 
-            Assert.AreEqual(HandState.Preflop, stateMachine.GetCurrentStateValue());
+            Assert.AreEqual(HandStage.Preflop, stateMachine.GetCurrentStage());
 
         }
     }
